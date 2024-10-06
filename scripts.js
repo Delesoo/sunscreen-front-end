623fd72027bef1f38d67bb24abf3a3b503b42c96
Delesoo <strongtoka@gmail.com>
2024-10-06 16:40:35 +0400

    sunscreen

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ document.querySelectorAll('.faq-item').forEach(item=> {
         item.classList.toggle('open');
     });
 });
+
+const hamburger = document.getElementById('hamburger-icon');
+const navMenu = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+    console.log("clicked!");
+    navMenu.classList.toggle('open');
+});
